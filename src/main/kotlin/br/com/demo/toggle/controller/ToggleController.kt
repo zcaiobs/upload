6c66c3da8cd75e2b774ad831f6c45f3e9c2912b7
zcaiobs <zcaiobernardodasilva@gmail.com>
2023-05-08 21:01:26 -0300

    feat: add download

--- src/main/kotlin/br/com/demo/toggle/controller/ToggleController.kt
+++ src/main/kotlin/br/com/demo/toggle/controller/ToggleController.kt
@@ -2,15 +2,20 @@ package br.com.demo.toggle.controller
 
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVParser
+import org.apache.commons.csv.CSVPrinter
 import org.apache.commons.csv.CSVRecord
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import org.togglz.core.manager.FeatureManager
 import org.togglz.core.util.NamedFeature
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import java.io.*
+import java.time.LocalDate
+import java.util.UUID
 
 @RestController
 @RequestMapping("/toggle")
@@ -38,6 +43,34 @@ class ToggleController {
         }
         return ResponseEntity.ok(listColor)
     }
+
+    @GetMapping("/csv/download")
+    fun handleFileDownload(): ResponseEntity<Any> {
+        val out = ByteArrayOutputStream()
+        val csvPrinter = CSVPrinter(PrintWriter(out), CSVFormat.DEFAULT)
+
+        csvPrinter.printRecord(getColunas())
+        getValues().forEach { csvPrinter.printRecord(it) }
+        csvPrinter.flush()
+
+        val file = InputStreamResource(ByteArrayInputStream(out.toByteArray()))
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + "clubes-report-${LocalDate.now()}.csv")
+                .contentType(MediaType.parseMediaType("application/csv"))
+                .body(file)
+    }
+}
+
+fun getColunas(): MutableList<String> {
+    return mutableListOf("id", "name", "description")
+}
+
+fun getValues(): MutableList<MutableList<String>> {
+    val list = mutableListOf<MutableList<String>>()
+    repeat(100) {
+        list.add(mutableListOf(UUID.randomUUID().toString(), "name ${UUID.randomUUID()}", "description ${UUID.randomUUID()}"))
+    }
+    return list
 }
 
 data class Color(val id: String, val name: String, val description: String, val teste: String, val hoje: String)
